769f2d5bf8214ff8db0f4f91c3cfe7c3c6fdd067
Siamak Irani <siamakirani1378@gmail.com>
2024-01-07 11:51:49 +0330

    Fixing bugs

---
+++ src/lib/type.ts
@@ -0,0 +1,11 @@
+import { IconType } from "react-icons/lib";
+
+export type EarningDataItem = {
+  icon: IconType;
+  amount: string;
+  percentage: string;
+  title: string;
+  iconColor: string;
+  iconBg: string;
+  pcColor: string;
+};
