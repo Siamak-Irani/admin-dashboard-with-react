1e61d09b542e88b1f4e7e0580ab6a48db66f2fc8
siamak irani <siamakirani1378@gmail.com>
2024-01-24 11:18:16 +0330

    Lazy loading added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import "./App.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import RootLayout from "./pages/RootLayout";
-import Ecommerce from "./pages/Ecommerce";
-import Orders from "./pages/Orders";
-import Employees from "./pages/Employees";
-import Customers from "./pages/Customers";
-import BigCalendar from "./pages/BigCalendar";
-import LineChartPage from "./pages/LineChartPage";
-import AreaChart from "./pages/AreaChartPage";
+
+const Ecommerce = lazy(() => import("./pages/Ecommerce"));
+const Orders = lazy(() => import("./pages/Orders"));
+const Employees = lazy(() => import("./pages/Employees"));
+const Customers = lazy(() => import("./pages/Customers"));
+const BigCalendar = lazy(() => import("./pages/BigCalendar"));
+const LineChartPage = lazy(() => import("./pages/LineChartPage"));
+const AreaChart = lazy(() => import("./pages/AreaChartPage"));
+
+// <Suspense fallback={<div></div>}></Suspense>
 
 const router = createBrowserRouter([
   {
@@ -17,15 +20,71 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Ecommerce />,
+        element: (
+          <Suspense fallback={<div></div>}>
+            <Ecommerce />
+          </Suspense>
+        ),
       },
-      { path: "ecommerce", element: <Ecommerce /> },
-      { path: "orders", element: <Orders /> },
-      { path: "employees", element: <Employees /> },
-      { path: "customers", element: <Customers /> },
-      { path: "calendar", element: <BigCalendar /> },
-      { path: "line", element: <LineChartPage /> },
-      { path: "area", element: <AreaChart /> },
+      {
+        path: "ecommerce",
+        element: (
+          <Suspense fallback={<div></div>}>
+            {" "}
+            <Ecommerce />
+          </Suspense>
+        ),
+      },
+      {
+        path: "orders",
+        element: (
+          <Suspense fallback={<div></div>}>
+            <Orders />
+          </Suspense>
+        ),
+      },
+      {
+        path: "employees",
+        element: (
+          <Suspense fallback={<div></div>}>
+            <Employees />{" "}
+          </Suspense>
+        ),
+      },
+      {
+        path: "customers",
+        element: (
+          <Suspense fallback={<div></div>}>
+            {" "}
+            <Customers />{" "}
+          </Suspense>
+        ),
+      },
+      {
+        path: "calendar",
+        element: (
+          <Suspense fallback={<div></div>}>
+            <BigCalendar />{" "}
+          </Suspense>
+        ),
+      },
+      {
+        path: "line",
+        element: (
+          <Suspense fallback={<div></div>}>
+            <LineChartPage />
+          </Suspense>
+        ),
+      },
+      {
+        path: "area",
+        element: (
+          <Suspense fallback={<div></div>}>
+            {" "}
+            <AreaChart />
+          </Suspense>
+        ),
+      },
     ],
   },
 ]);
